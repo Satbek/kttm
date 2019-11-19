97d84c6ce57e131e8ae79f8bf08539582c758dc7
Satbek Turganbayev <s.turganbayev@corp.mail.ru>
2019-11-20 00:16:23 +0300

    GUI simplified.

--- view.py
+++ view.py
@@ -3,179 +3,185 @@ import random
 import numpy as np
 from PyQt5 import QtCore, QtWidgets, QtGui
 
-#todo clean up
-
+#todo via Qt transform
 class Arrow(QtWidgets.QGraphicsLineItem):
-    def __init__(self, x, y, radius, outerPoint):
+    def __init__(self, x, y, radius, degree):
         self.radius = radius
         self.start = QtCore.QPointF(x, y)
-        AgngleCoef = self._getAngleCoef(outerPoint)
+        rad = np.deg2rad(degree)
         super().__init__(
-            x + radius * AgngleCoef[0],
-            y + radius * AgngleCoef[1],
-            x + 3 * radius * AgngleCoef[0],
-            y + 3 * radius * AgngleCoef[1]
-        )
-        self.vector = self._makeVector(radius, AgngleCoef)
-        self.setAcceptHoverEvents(True)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
-        self.setTransformOriginPoint(
-            x + radius * AgngleCoef[0],
-            y + radius * AgngleCoef[1],
+            x + radius * np.sin(rad),
+            y + radius * np.cos(rad),
+            x + 3 * radius * np.sin(rad),
+            y + 3 * radius * np.cos(rad)
         )
 
-    def mouseMoveEvent(self, event):
-        self.setVector(event.pos())
-
-    def setVector(self, outerPoint):
-        AgngleCoef = self._getAngleCoef(outerPoint)
+    def setVector(self, degree):
+        rad = np.deg2rad(degree)
         x, y, radius = self.start.x(), self.start.y(), self.radius
-        self.vector = self._makeVector(radius, AgngleCoef)
         self.setLine(
-            x + radius * AgngleCoef[0],
-            y + radius * AgngleCoef[1],
-            x + 3 * radius * AgngleCoef[0],
-            y + 3 * radius * AgngleCoef[1]
+            x + radius * np.sin(rad),
+            y + radius * np.cos(rad),
+            x + 3 * radius * np.sin(rad),
+            y + 3 * radius * np.cos(rad)
         )
-        self.setTransformOriginPoint(
-            x + radius * AgngleCoef[0],
-            y + radius * AgngleCoef[1],
-        )
-    
-    def _makeVector(self, radius, AgngleCoef):
-        x, y = self.start.x(), self.start.y()
-        return QtCore.QPointF(x + radius * AgngleCoef[0], y + radius * AgngleCoef[1])
-
-    def _getAngleCoef(self, outerPoint):
-        """
-        Get sin, cos coeffs to draw line toward provided point
-        """
-        norm = np.sqrt(outerPoint.x()**2 + outerPoint.y()**2)
-        norm_x = 1 if norm == 0 else outerPoint.x() / norm
-        norm_y = 1 if norm == 0 else outerPoint.y() / norm
-        return (norm_x, norm_y)
-
 
 class Emitter(QtWidgets.QGraphicsEllipseItem):
-    def _buildArrowPen(self):
-        arrowPen = QtGui.QPen()
-        arrowPen.setWidth(3)
-        arrowPen.setBrush(QtCore.Qt.red)
-        self.arrow.setPen(arrowPen)
-
-    def _buildPen(self):
-        selfPen = QtGui.QPen()
-        selfPen.setWidth(3)
-        selfPen.setBrush(QtCore.Qt.red)
-        self.setPen(selfPen)
-
-    def __init__(self, radius, vector):
+    def __init__(self, radius, degree, spawnInterval):
+        """
+        radius - emitter circle radius
+        degree - arrow rotation degree (0, 360)
+        spawnInterval - (a, b) interval. Between this interval emitter will
+        spawn particle
+        """
         super().__init__(
             QtCore.QRectF(-1 * radius, -1 * radius, 2 * radius, 2 * radius)
         )
         self.radius = radius
+        self.degree = degree
+        self.spawnInterval = spawnInterval
         self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
-        self.arrow = Arrow(self.x(), self.y(), radius, vector)
+        self.arrow = Arrow(self.x(), self.y(), radius, degree)
         self.arrow.setParentItem(self)
-
-        self._buildPen()
-        self._buildArrowPen()
-
-    def getVector(self):
-        return self.arrow.vector
-
-
-class Particle(QtWidgets.QGraphicsEllipseItem):
-    def __init__(self, radius, vector):
-        super().__init__(
-            QtCore.QRectF(-1 * radius, -1 * radius, 2 * radius, 2 * radius)
+        self.particleMass = 10
+        self.particle_speed_x = 0
+        self.particle_speed_y = 0
+        self.double_validator = QtGui.QDoubleValidator(
+            -1000000000000.0, 100000000000.0, 3
         )
-        self.radius = radius
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsSelectable, True)
-        self.arrow = Arrow(self.x(), self.y(), radius, vector)
-        self.arrow.setParentItem(self)
-
-    def setScale(self, param):
-        super().setScale(param)
-
-    def setColor(self):
-        pass
-
-    def getVector(self):
-        return self.arrow.vector
-
-class ParticleSizeSlider(QtWidgets.QSlider):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setRange(0, 100)
-        self.setTickInterval(0.5)
-        self.setSingleStep(0.5)
-        self.particle = None
-
-
-class MainWidget(QtWidgets.QWidget):
-    def _buildInsrumentLayout(self):
-        insrumentLayout = QtWidgets.QVBoxLayout()
-        spawn_button = QtWidgets.QPushButton("Spawn particle")
-        spawn_button.clicked.connect(self.addParticle)
-
-        clear_button = QtWidgets.QPushButton("Clear")
-        clear_button.clicked.connect(self.Clear)
-
-        buttons_layout = QtWidgets.QHBoxLayout()
-        buttons_layout.addWidget(spawn_button)
-        buttons_layout.addWidget(clear_button)
-
-        insrumentLayout.addLayout(buttons_layout)
-
-        self.slider = ParticleSizeSlider(QtCore.Qt.Horizontal)
-        self.slider.valueChanged.connect(self.resizeItems)
-        insrumentLayout.addWidget(self.slider)
-        
-        return insrumentLayout
 
     QtCore.pyqtSlot(int)
-    def resizeItems(self, value):
-        for item in self.view.scene().selectedItems():
-            item.setScale(value / 20)
+    def setVector(self, degree):
+        self.degree = degree
+        self.arrow.setVector(degree)
 
-    def addEmitter(self):
-        emitter = Emitter(20, QtCore.QPoint(1, 1))
-        self.emitter = emitter
-        self.view.scene().addItem(emitter)
+    QtCore.pyqtSlot(float)
+    def setParticleMass(self, mass):
+        self.particleMass = mass
 
-    def _buildView(self):
-        scene = QtWidgets.QGraphicsScene()
-        self.view = QtWidgets.QGraphicsView(scene)
-        self.addEmitter()
+    QtCore.pyqtSlot(float)
+    def setParticleSpeedX(self, speed_x):
+        if self.double_validator.validate(speed_x, 0)[0] == QtGui.QValidator.Acceptable:
+            self.particle_speed_x = float(speed_x.replace(",", "."))
 
-    @QtCore.pyqtSlot()
-    def Clear(self):
-        self.view.scene().clear()
-        self.addEmitter()
+    QtCore.pyqtSlot(float)
+    def setParticleSpeedY(self, speed_y):
+        if self.double_validator.validate(speed_y, 0)[0] == QtGui.QValidator.Acceptable:
+            self.particle_speed_y = float(speed_y.replace(",", "."))
 
-    @QtCore.pyqtSlot()
-    def addParticle(self):
-        particle = Particle(self.baseParticleSize, QtCore.QPointF(1, 1))
-        direction = self.emitter.getVector()
-        shift = random.randint(5, 25)
-
-        particle.setPos(
-            self.emitter.x() + direction.x() * shift,
-            self.emitter.y() + direction.y() * shift
+class Particle(QtWidgets.QGraphicsEllipseItem):
+    def __init__(self, radius):
+        super().__init__(
+            QtCore.QRectF(-1 * radius, -1 * radius, 2 * radius, 2 * radius)
         )
-        self.view.scene().addItem(particle)
+        self.radius = radius
+
+class UniverseView(QtWidgets.QGraphicsView):
+    emitter_x_changed_signal = QtCore.pyqtSignal(str)
+    emitter_y_changed_signal = QtCore.pyqtSignal(str)
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.emitter = Emitter(10, 0, (10, 40))
+        self.scene().addItem(self.emitter)
+        self.particles = []
+
+    @staticmethod
+    def mass2radius(mass):
+        return mass
+
+    QtCore.pyqtSlot()
+    def addParticle(self):
+        particle = Particle(self.mass2radius(self.emitter.particleMass))
+        distance = np.random.randint(*self.emitter.spawnInterval)
+        x = self.emitter.x() + (
+                self.emitter.radius + distance
+            ) * np.sin(np.deg2rad(self.emitter.degree))
+        
+        y = self.emitter.y() + (
+                self.emitter.radius + distance
+            ) * np.cos(np.deg2rad(self.emitter.degree))
+        particle.setPos(x, y)
+        self.particles.append(particle)
+        self.scene().addItem(particle)
+
+class MainWidget(QtWidgets.QWidget):
+    def buildEmitterLayout(self):
+        self.emitter_vector_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.emitter_vector_slider.setRange(0, 360)
+        self.emitter_vector_slider.setTickInterval(1)
+        self.emitter_vector_slider.setSingleStep(1)
+        layout = QtWidgets.QHBoxLayout()
+        layout.addWidget(self.emitter_vector_slider)
+
+        self.particle_size_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.particle_size_slider.setRange(10, 100)
+        self.particle_size_slider.setTickInterval(1)
+        self.particle_size_slider.setSingleStep(1)
+        layout.addWidget(self.particle_size_slider)
+
+        double_validator = QtGui.QDoubleValidator(
+            -1000000000000.0, 100000000000.0, 3
+        )
+        self.particle_speed_x_line_edit = QtWidgets.QLineEdit()
+        self.particle_speed_x_line_edit.setValidator(double_validator)
+        self.particle_speed_x_line_edit.setText("0,0")
+
+        self.particle_speed_y_line_edit = QtWidgets.QLineEdit()
+        self.particle_speed_y_line_edit.setValidator(double_validator)
+        self.particle_speed_y_line_edit.setText("0,0")
+
+        layout.addWidget(self.particle_speed_x_line_edit)
+        layout.addWidget(self.particle_speed_y_line_edit)
+        return layout
+
+    def buildInsrumentLayout(self):
+        insrumentLayout = QtWidgets.QVBoxLayout()
+        insrumentLayout.addLayout(self.buildEmitterLayout())
+        self.spawn_particle_button = QtWidgets.QPushButton("Spawn particle")
+        self.animation_start_button = QtWidgets.QPushButton("Start")
+        self.animation_stop_button = QtWidgets.QPushButton("Stop")
+        insrumentLayout.addWidget(self.spawn_particle_button)
+        insrumentLayout.addWidget(self.animation_start_button)
+        insrumentLayout.addWidget(self.animation_stop_button)
+        return insrumentLayout
+
+    def buildView(self):
+        scene = QtWidgets.QGraphicsScene()
+        self.view = UniverseView(scene)
+        return self.view
+
+    def buildUI(self):
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.buildView())
+        self.layout.addLayout(self.buildInsrumentLayout())
+        self.setLayout(self.layout)
+
+    def buildConnections(self):
+        self.emitter_vector_slider.valueChanged.connect(
+            self.view.emitter.setVector
+        )
+
+        self.particle_size_slider.valueChanged.connect(
+            self.view.emitter.setParticleMass
+        )
+
+        self.spawn_particle_button.clicked.connect(
+            self.view.addParticle
+        )
+
+        self.particle_speed_x_line_edit.textChanged.connect(
+            self.view.emitter.setParticleSpeedX
+        )
+
+        self.particle_speed_x_line_edit.textChanged.connect(
+            self.view.emitter.setParticleSpeedX
+        )
+
+        self.particle_speed_y_line_edit.textChanged.connect(
+            self.view.emitter.setParticleSpeedY
+        )
 
     def __init__(self):
         super().__init__()
-        self.baseParticleSize = 10
-
-        self.layout = QtWidgets.QHBoxLayout()
-
-        self._buildView()
-        self.layout.addWidget(self.view)
-
-        self.layout.addLayout(self._buildInsrumentLayout())
-
-        self.setLayout(self.layout)
+        self.buildUI()
+        self.buildConnections()
