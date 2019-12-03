ceb4567dd9ae1e03636ddfc6be18bc045ce833dc
Satbek Turganbayev <s.turganbayev@corp.mail.ru>
2019-12-04 02:04:10 +0300

    Add ability to select particle color

--- viewModel/particle.py
+++ viewModel/particle.py
@@ -1,13 +1,13 @@
 from PyQt5 import QtCore, QtWidgets, QtGui
 
 class Particle(QtCore.QObject):
-    def __init__(self, radius, model):
+    def __init__(self, radius, model, color = QtGui.QColor("black")):
         super().__init__()
         self.radius = radius
         self.item = QtWidgets.QGraphicsEllipseItem(
             QtCore.QRectF(-1 * radius, -1 * radius, 2 * radius, 2 * radius)
         )
-        self.item.setBrush(QtGui.QBrush(QtCore.Qt.black))
+        self.item.setBrush(color)
         self.model = model
         self._set_pos(QtCore.QPointF(model.x, model.y))
 
